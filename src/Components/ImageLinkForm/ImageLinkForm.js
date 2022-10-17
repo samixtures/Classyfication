53ca8f65fe91688ebfb0afbf772391b4c2fcf244
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-10-16 22:51:37 -0700

    Upload button seems to work!

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,8 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit, sendBytes}) => {
                 <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2 w-70 center" type = "text" onChange={onInputChange}/>
                     <button onClick = {onButtonSubmit} className="w-30 f4 grow link ph3 pv2 dib white bg-light-green">Detect</button>
-                    <input type="file" className="custom-file-input" id="image-input" accept="image/jpeg, image/png, image/jpg" onChange = {event => sendBytes(event)}></input>
+                    <label htmlFor="image-input" className="w-15 f4 grow link ph3 pv2 dib white bg-light-red"> Upload</label>
+                    <input name = "myfile" type="file" className="custom-file-input" id="image-input" accept="image/jpeg, image/png, image/jpg" onChange = {event => sendBytes(event)}></input>
                 </div>
             </div>
 
